d6b6690c7ff5f773046a71fa84c929ba472c92a6
AnnyaB <anabordin2008@gmail.com>
2024-04-22 21:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿// 1. faça um programa que leia um número inteiro e imprima até esta posição da sequencia fibonacci;
+
+int numero = 0, n1 = 1, n2 = 1, aux = 0;
+
+do
+{
+    Console.WriteLine("Digite um número de elementos:");
+    numero = int.Parse(Console.ReadLine());
+} while (numero < 1);
+
+int[] fibonacci = new int[numero];
+
+Console.WriteLine("Sequencia Fibonacci:");
+
+if (numero >= 1)
+{
+    fibonacci[0] = n1;
+}
+
+if (numero >= 2)
+{
+    fibonacci[1] = n2;
+}
+
+if (numero >= 3)
+{
+    for (int i = 2; i < numero; i++)
+    {
+        aux = n1 + n2;
+        n1 = n2;
+        n2 = aux;
+        fibonacci[i] = aux;
+    }
+}
+
+for (int i = 0; i < numero; i++)
+{
+    Console.Write($"{fibonacci[i]}, ");
+}
